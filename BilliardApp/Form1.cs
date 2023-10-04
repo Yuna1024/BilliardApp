360f0abdc0122b3feb7fc7ce493e417d18ed9832
Nguyễn Qúy Dương <129517457+Yuna1024@users.noreply.github.com>
2023-10-04 21:15:53 +0700

    Duong update

--- BilliardApp/Form1.cs
+++ BilliardApp/Form1.cs
@@ -8,7 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
-
+//Tung ngu
 namespace BilliardApp
 {
     public partial class Form1 : Form
